618d33e05f63611f93b1724e72745f3e59c55c52
DuckDan <36496107+DanielAtella@users.noreply.github.com>
2023-12-28 16:41:30 +0000

    a

--- bgp.tf
+++ bgp.tf
@@ -12,3 +12,34 @@ resource "aws_bgp_route_table" "route_table" {
     ipv4_prefix = each.value
   }
 }
+
+resource "fortimanager_json_generic_api" "Create_Vlan_100" {
+    json_content = <<JSON
+  {
+      "method": "add",
+      "params": [
+          {
+            "url": "/pm/config/device/FGT2-N1/global/system/interface",
+              "data": {
+                "name": "VL_100",
+                "vdom": [
+                    "FG-traffic"
+                ],
+                "mode": "static",
+                "ip": [
+                    "10.10.10.10",
+                    "255.255.255.0"
+                ],
+                "allowaccess": "ping",
+                "type": "vlan",
+                "vlanid": 100,
+                "vlan-protocol": "8021q",
+                "interface": [
+                    "LAG"
+                ]
+            }
+          }
+      ]
+  }
+  JSON
+  }
